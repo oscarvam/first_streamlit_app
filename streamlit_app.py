912a66e33d76f9256f13d3854619d8969665af06
oscarvam <97786046+oscarvam@users.noreply.github.com>
2022-12-04 17:39:13 +0100

    Update streamlit_app.py
    added emojis and new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Menú de desayuno')
-streamlit.text('Omega 3 y avena con arándanos')
-streamlit.text('Batido de col rizada, espinacas y rúcula')
-streamlit.text('Huevo de gallinas camperas hervidas')
+streamlit.text(🥣'Omega 3 y avena con arándanos')
+streamlit.text(🥗'Batido de col rizada, espinacas y rúcula')
+streamlit.text(🥚'Huevo de gallinas camperas hervidas')
+streamlit.header('🍌🥭 Crea tu propio batido de frutas 🥝🍇')
