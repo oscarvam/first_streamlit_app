e26e3874b7bbe9a202e1eeb5cd6fe3e019d3f2b3
oscarvam <97786046+oscarvam@users.noreply.github.com>
2022-12-04 17:42:46 +0100

    Update streamlit_app.py
    added pandas dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
+import pandas
+
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -6,4 +8,7 @@ streamlit.header('Menú de desayuno')
 streamlit.text('🥣Omega 3 y avena con arándanos')
 streamlit.text('🥗Batido de col rizada, espinacas y rúcula')
 streamlit.text('🥚Huevo de gallinas camperas hervidas')
+
 streamlit.header('🍌🥭 Crea tu propio batido de frutas 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
